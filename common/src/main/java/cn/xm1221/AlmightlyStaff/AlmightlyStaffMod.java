65238eb077aaf5dd218a8934b6552e41c45b93bf
xm1221 <3850632202@qq.com>
2026-06-14 09:37:54 +0800

    Revert "use old api"
    This reverts commit 69795752728e3231ad4bfad9856f8ef88233b347.

--- common/src/main/java/cn/xm1221/AlmightlyStaff/AlmightlyStaffMod.java
+++ common/src/main/java/cn/xm1221/AlmightlyStaff/AlmightlyStaffMod.java
@@ -6,11 +6,12 @@ import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 
 public final class AlmightlyStaffMod {
     public static final String MOD_ID = "almightly_staff";
 
-    public static final Item ALL_IN_ONE = new ItemAlmightlyStaff(new Item.Properties().stacksTo(1));
+    public static final Item ALL_IN_ONE = new ItemAlmightlyStaff(new Item.Properties().rarity(Rarity.UNCOMMON).stacksTo(1));
 
     public static void init() {
         // Write common init code here.
